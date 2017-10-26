4078dd0f7f66183ddd6147a8a35e7ca874cdd16d
de Castro <adecastro@ea.com>
2017-10-26 11:06:08 +0200

    Updates on modeling and data functions. Data : added a JSON deparsing functions to map itemid,championid etc... also added a function to create a "team" dataset by aggregating the player one Modeling :added two brute force binomial glm with the complete dataset. 98% recall/precision/prediction accuracy on the team dataset

--- models.R
+++ models.R
@@ -4,3 +4,54 @@ PositionGoldLinearModel <- function(players.dt){
   model <- glm(data=model.dt,formula = win ~ goldearnedpersecond:role,family='binomial')
   return(model)
 }
+
+SimpleClassification <- function(){
+  temp.dt <- players.dt[, .(TotalGold = sum(as.numeric(goldearned))),.(matchid,win)]
+  temp.dt[,Team:=ifelse(win==1,'WinningTeam','LosingTeam')]
+  temp.dt <- dcast(temp.dt[,.(Team,matchid,TotalGold)],matchid~Team,value.var='TotalGold')
+  temp.dt[,GoldHigherForWinning:=WinningTeam>LosingTeam]
+  cat('Winning team had more gold than the losing team',nrow(temp.dt[GoldHigherForWinning==T])/nrow(temp.dt)*100,'% of the time')
+}
+
+CompleteTeamModel <- function(teams.dt){
+  model <- glm(data=teams.dt[,!c("matchid","teamid")],formula= win~.,family='binomial')
+  
+  validation <- as.data.table(cbind(teams.dt[,win],model$fitted.values))
+  setnames(validation,c('Actual','WinningProbality'))
+  validation[,ActualFactor:=factor(Actual,levels=c('0','1'))]
+  
+  #boxplots of probability, not very interesting as it's pretty much only outliers
+  plot1 <- ggplot(data = validation) + geom_boxplot(aes(x = ActualFactor,y = WinningProbality))
+  print(plot1)
+  
+  validation[,PredictedWin := 1*(WinningProbality>0.5)]
+  
+  cat('Prediction accuracy :',nrow(validation[PredictedWin==Actual]) / nrow(validation),'\n')
+  cat('Recall :',nrow(validation[PredictedWin==1 & PredictedWin==Actual]) / nrow(validation[PredictedWin==1 ]),'\n')
+  cat('Precision :',nrow(validation[PredictedWin==1 & PredictedWin==Actual]) / nrow(validation[Actual==1]))
+  
+  return(model)
+  
+}
+
+CompletePlayerModel <- function(players.dt){
+  model.data <- players.dt[,!c("matchid","teamid","player","id","championid")]
+  model <- glm(data=players.dt[,!c("matchid","teamid","player")],formula= win~.,family='binomial')
+  
+  validation <- as.data.table(cbind(players.dt[,win],model$fitted.values))
+  setnames(validation,c('Actual','WinningProbality'))
+  validation[,ActualFactor:=factor(Actual,levels=c('0','1'))]
+  
+  #boxplots of probability, not very interesting as it's pretty much only outliers
+  plot1 <- ggplot(data = validation) + geom_boxplot(aes(x = ActualFactor,y = WinningProbality))
+  print(plot1)
+  
+  validation[,PredictedWin := 1*(WinningProbality>0.5)]
+  
+  cat('Prediction accuracy :',nrow(validation[PredictedWin==Actual]) / nrow(validation),'\n')
+  cat('Recall :',nrow(validation[PredictedWin==1 & PredictedWin==Actual]) / nrow(validation[PredictedWin==1 ]),'\n')
+  cat('Precision :',nrow(validation[PredictedWin==1 & PredictedWin==Actual]) / nrow(validation[Actual==1]))
+  
+  return(model)
+  
+}
--- read.data.R
+++ read.data.R
@@ -1,6 +1,9 @@
+#loading packages and sourcing external files
 library(data.table)
 library(ggplot2)
 library(scales)
+library(leaps)
+library(rjson)
 source('plot.functions.R')
 source('models.R')
 
@@ -13,26 +16,124 @@ ReadData <- function(){
   teamstats.dt <<- fread('teamstats.csv')
 }
 
-Formatdata <- function(players.dt){
+GetJsonFiles <- function(){
+  #this function parses the Json files directly from the data dragon LoL API website and formats them into a R list.
+  #it allows us to map the itemid, summoner spell id and champion id to their corresponding name.
+  
+  summoner.json <- fromJSON(file='http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/summoner.json')
+  summoner.dt <- c()
+  for(obj in summoner.json$data){
+    summoner.dt <-rbind(summoner.dt,c(obj$name,obj$key))
+  }
+  summoner.dt <- as.data.table(summoner.dt)
+  setnames(summoner.dt,c('Name','id'))
+  
+  champion.json <- fromJSON(file='http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
+  champion.dt <- c()
+  for(obj in champion.json$data){
+    champion.dt <- rbind(champion.dt,c(obj$name,obj$key))
+  }
+  champion.dt <- as.data.table(champion.dt)
+  setnames(champion.dt,c('Name','id'))
+  
+  item.json <- fromJSON(file='http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/item.json')
+  item.dt <- c()
+  for(obj in names(item.json$data)){
+    item.dt <- rbind(item.dt,c(item.json$data[[obj]]$name,as.numeric(obj)))
+  }
+  item.dt <- as.data.table(item.dt)
+  setnames(item.dt,c('Name','id'))
+  
+  return(list('item' = item.dt,'summoner' = summoner.dt, 'champions' = champion.dt))
+}
+
+FormatPlayerData <- function(players.dt){
   #fixes the weird '\N' character to NA and converts the rest back to integers
   players.dt[wardsbought=='\\N',wardsbought := NA]
   players.dt[,wardsbought := as.integer(wardsbought)]
+  
   #adding duration for ease of computation later on
-  players.dt <- merge(players.dt,match.dt[,.(id,duration)],by.x='matchid',by.y='id')
+  players.dt <- merge(players.dt,match.dt[,.(id,duration,queueid)],by.x='matchid',by.y='id')
+  
+  #adding the teamid to merge easily with the other datasets
+  players.dt[player <=5,teamid := 100]
+  players.dt[player >=6,teamid := 200]
+  
+  #removing the 3v3 games and the weird 3 games with only 8 and 9players(i.e. the 3 participants missing above)
+  players.dt <- players.dt[queueid %in% c(420,440)]
+  players.dt <- players.dt[!(matchid %in% players.dt[,.N,.(matchid)][N<10]$matchid)]
+  
+  #diagnostic to check I did it right :) 
+  # testing <- merge(teamstats.dt[,.(matchid,teamid,firstblood)],players.dt[,sum(firstblood),.(matchid,teamid)],by=c("matchid","teamid"))
+  # players.dt[matchid %in% testing[firstblood!=V1]$matchid, .N,.(matchid)]
+  # players.dt[, .N,.(matchid)][,.N,N]
+  
   return(players.dt)
 }
 
-SimpleClassification <- function(){
-  temp.dt <- players.dt[, .(TotalGold = sum(as.numeric(goldearned))),.(matchid,win)]
-  temp.dt[,Team:=ifelse(win==1,'WinningTeam','LosingTeam')]
-  temp.dt <- dcast(temp.dt[,.(Team,matchid,TotalGold)],matchid~Team,value.var='TotalGold')
-  temp.dt[,GoldHigherForWinning:=WinningTeam>LosingTeam]
-  cat('Winning team had more gold than the losing team',nrow(temp.dt[GoldHigherForWinning==T])/nrow(temp.dt)*100,'% of the time')
+CreateTeamData <- function(players.dt){
+  #create the "team" dataset, i.e. we aggregate the players metric to a team level, by summing, averaging or taking the min max.
+  team.dt <- players.dt[,list(
+      kills = sum(kills,na.rm=T),
+      deaths = sum(deaths,na.rm=T),
+      assists = sum(assists,na.rm=T),
+      largestkillingspree = sum(largestkillingspree,na.rm=T),
+      largestmultikill = sum(largestmultikill,na.rm=T),
+      killingsprees = sum(killingsprees,na.rm=T),
+      longesttimespentliving = sum(longesttimespentliving,na.rm=T),
+      doublekills = sum(doublekills,na.rm=T),
+      triplekills = sum(triplekills,na.rm=T),
+      quadrakills = sum(quadrakills,na.rm=T),
+      pentakills = sum(pentakills,na.rm=T),
+      legendarykills = sum(legendarykills,na.rm=T),
+      totdmgdealt = sum(totdmgdealt,na.rm=T),
+      magicdmgdealt = sum(magicdmgdealt,na.rm=T),
+      physicaldmgdealt = sum(physicaldmgdealt,na.rm=T),
+      truedmgdealt = sum(truedmgdealt,na.rm=T),
+      largestcrit = max(largestcrit,na.rm=T),
+      totdmgtochamp = sum(totdmgtochamp,na.rm=T),
+      magicdmgtochamp = sum(magicdmgtochamp,na.rm=T),
+      physdmgtochamp = sum(physdmgtochamp,na.rm=T),
+      truedmgtochamp = sum(truedmgtochamp,na.rm=T),
+      totheal = sum(totheal,na.rm=T),
+      totunitshealed = sum(totunitshealed,na.rm=T),
+      dmgselfmit = sum(dmgselfmit,na.rm=T),
+      dmgtoobj = sum(dmgtoobj,na.rm=T),
+      dmgtoturrets = sum(dmgtoturrets,na.rm=T),
+      visionscore = sum(visionscore,na.rm=T),
+      timecc = sum(timecc,na.rm=T),
+      totdmgtaken = sum(totdmgtaken,na.rm=T),
+      magicdmgtaken = sum(magicdmgtaken,na.rm=T),
+      physdmgtaken = sum(physdmgtaken,na.rm=T),
+      truedmgtaken = sum(truedmgtaken,na.rm=T),
+      goldearned = sum(goldearned,na.rm=T),
+      goldspent = sum(goldspent,na.rm=T),
+      totminionskilled = sum(totminionskilled,na.rm=T),
+      neutralminionskilled = sum(neutralminionskilled,na.rm=T),
+      ownjunglekills = sum(ownjunglekills,na.rm=T),
+      enemyjunglekills = sum(enemyjunglekills,na.rm=T),
+      totcctimedealt = sum(totcctimedealt,na.rm=T),
+      maxchamplvl = max(champlvl,na.rm=T),
+      avgchamplvl = sum(champlvl,na.rm=T)/5,
+      minchamplvl = min(champlvl,na.rm=T),
+      pinksbought = sum(pinksbought,na.rm=T),
+      wardsplaced = sum(wardsplaced,na.rm=T),
+      wardskilled = sum(wardskilled,na.rm=T)
+  ),.(matchid,teamid,duration,win)]
+  
+  #get team specific metric from teamstats
+  team.dt <- merge(teamstats.dt,team.dt,by=c('matchid','teamid'))
+  
+  return(team.dt)
 }
+
 
 #### Execution ####
 
-# ReadData()
-# players.dt <- Formatdata(players.dt)
+ReadData()
+players.dt <- FormatPlayerData(players.dt)
+teams.dt <- CreateTeamData(players.dt)
+id.mapping.list <- GetJsonFiles()
 
-model1 <- PositionGoldLinearModel(players.dt)
+#model.team <- CompleteTeamModel(teams.dt)
+#model.player <- CompletePlayerModel(players.dt)
